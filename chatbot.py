c9c09967035e6cec8b313454050861b123ed056b
TasiaCode2 <49112029+TasiaCode2@users.noreply.github.com>
2023-07-15 09:17:53 -0600

    Agregar mas patrones de respuesta

--- chatbot.py
+++ chatbot.py
@@ -4,31 +4,39 @@ from nltk.chat.util import Chat, reflections
 # Definición de los patrones de entrada y respuestas
 patrones = [
     [
-        r"mi nombre es (.*)",  # Si el usuario dice "mi nombre es [nombre]"
-        ["Hola %1, ¿cómo puedo ayudarte?",]  # El chatbot responde saludando con el nombre del usuario
+        r"hola|buenos dias|buenas tardes|buenas noches",
+        ["¡Hola! ¿Como te llamas?",]
     ],
     [
-        r"hola|buenos dias|buenas tardes|buenas noches",  # Si el usuario dice "hola" o saludo similar
-        ["¡Hola! ¿En qué puedo ayudarte?",]  # El chatbot responde con un saludo y ofrece su ayuda
+        r"mi nombre es (.*)|me llamo (.*)",
+        ["Hola %1, ¿cómo puedo ayudarte?",]  
     ],
     [
         r"¿Cómo estás?",  # Si el usuario pregunta "¿Cómo estás?"
         ["Estoy bien, gracias. ¿Y tú?",]  # El chatbot responde que está bien y pregunta cómo está el usuario
     ],
     [
-        r"bye|chao|adios|diu|hasta luego|Hasta Luego",  # Si el usuario dice "bye" o algo similar
-        ["¡Hasta luego! Si necesitas algo más, no dudes en preguntar.",]  # El chatbot se despide y finaliza la conversación
+        r"Me siento en peligro|Necesito ayuda",
+        ["Recuerda llamar a las líneas de emergencia si es una urgencia: 911"]
+    ],
+    [
+        r"Quiero platicar con alguien",
+        ["Claro! Aquí estoy para escucharte. ¿Sobre qué quieres hablar?"]
+    ]
+    [
+        r"bye|chao|adios|diu|hasta luego|Hasta Luego",
+        ["¡Hasta luego! Si necesitas algo más, no dudes en preguntar.",]
     ],
     [
         r"(|)",
-        ["Lo siento, no puedo procesar tu mensaje"]
+        ["Lo siento, no puedo procesar tu mensaje :("]
     ]
 ]
 
 def chatbot():
     print("¡Hola! Soy un chatbot. ¿En qué puedo ayudarte?")
-    chat = Chat(patrones, reflections)  # Creación de un objeto Chat con los pares de entrada y respuestas
-    chat.converse()  # Inicio de la conversación con el chatbot
+    chat = Chat(patrones, reflections)
+    chat.converse()
 
 if __name__ == "__main__":
-    chatbot()  # Llamada a la función principal para iniciar el chatbot
+    chatbot()
